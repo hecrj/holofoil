5aabdfa844f34c209a51eea3b908e065a0aad765
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-09-04 09:16:36 +0200

    Draft initial `holofoil` viewer

---
+++ src/lib.rs
@@ -0,0 +1,369 @@
+pub use bytes::Bytes;
+pub use wgpu;
+
+use std::mem;
+
+#[derive(Debug)]
+pub struct Pipeline {
+    raw: wgpu::RenderPipeline,
+    uniforms: wgpu::Buffer,
+    uniforms_binding: wgpu::BindGroup,
+    textures_layout: wgpu::BindGroupLayout,
+    back_texture: wgpu::Texture,
+}
+
+macro_rules! load_wgsl {
+    ($path:literal) => {
+        if cfg!(debug_assertions) {
+            $crate::wgpu::ShaderModuleDescriptor {
+                label: Some($path),
+                source: $crate::wgpu::ShaderSource::Wgsl(
+                    ::std::fs::read_to_string(format!(
+                        "{}/src/{}",
+                        env!("CARGO_MANIFEST_DIR"),
+                        $path
+                    ))
+                    .unwrap()
+                    .into(),
+                ),
+            }
+        } else {
+            $crate::wgpu::include_wgsl!($path)
+        }
+    };
+}
+
+impl Pipeline {
+    pub fn new(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        format: wgpu::TextureFormat,
+        back_texture: Layer,
+    ) -> Self {
+        let uniforms = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("holofoil uniforms buffer"),
+            size: mem::size_of::<Uniforms>() as u64,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            label: Some("holofoil sampler"),
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
+            ..wgpu::SamplerDescriptor::default()
+        });
+
+        let back_texture = back_texture.upload(device, queue);
+
+        let uniforms_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("holofoil uniforms layout"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX | wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+            ],
+        });
+
+        let uniforms_binding = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &uniforms_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+                        buffer: &uniforms,
+                        offset: 0,
+                        size: None,
+                    }),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&sampler),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: wgpu::BindingResource::TextureView(
+                        &back_texture.create_view(&wgpu::TextureViewDescriptor::default()),
+                    ),
+                },
+            ],
+        });
+
+        let textures_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("holofoil texture layout"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+            ],
+        });
+
+        let layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("holofoil pipeline layout"),
+            bind_group_layouts: &[&uniforms_layout, &textures_layout],
+            push_constant_ranges: &[],
+        });
+
+        let shader = device.create_shader_module(load_wgsl!("./shader.wgsl"));
+
+        let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("holofoil pipeline"),
+            layout: Some(&layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: Some("vs_main"),
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+                buffers: &[wgpu::VertexBufferLayout {
+                    array_stride: mem::size_of::<Instance>() as u64,
+                    step_mode: wgpu::VertexStepMode::Instance,
+                    attributes: &wgpu::vertex_attr_array!(
+                        // Position
+                        0 => Float32x2,
+                        // Size
+                        1 => Uint32x2,
+                        // Rotation
+                        2 => Float32,
+                    ),
+                }],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: Some("fs_main"),
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format,
+                    blend: Some(wgpu::BlendState::ALPHA_BLENDING),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState::default(),
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState::default(),
+            multiview: None,
+            cache: None,
+        });
+
+        Self {
+            raw: pipeline,
+            uniforms,
+            uniforms_binding,
+            textures_layout,
+            back_texture,
+        }
+    }
+
+    pub fn upload(
+        &self,
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        definition: Structure,
+    ) -> Result<Card, wgpu::Error> {
+        let instance = device.create_buffer(&wgpu::wgt::BufferDescriptor {
+            label: Some("holofoil instance buffer"),
+            size: mem::size_of::<Instance>() as u64,
+            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let base = definition.base.upload(device, queue);
+        let foil = definition.foil.map(|foil| foil.upload(device, queue));
+        let etching = definition
+            .etching
+            .map(|etching| etching.upload(device, queue));
+
+        let base_view = base.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let foil_view = foil
+            .as_ref()
+            .map(|foil| foil.create_view(&wgpu::TextureViewDescriptor::default()));
+
+        let etching_view = etching
+            .as_ref()
+            .map(|etching| etching.create_view(&wgpu::TextureViewDescriptor::default()));
+
+        let binding = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &self.textures_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&base_view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::TextureView(
+                        &foil_view.unwrap_or(base_view.clone()),
+                    ),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: wgpu::BindingResource::TextureView(
+                        &etching_view.unwrap_or(base_view.clone()),
+                    ),
+                },
+            ],
+        });
+
+        Ok(Card {
+            instance,
+            base,
+            foil,
+            etching,
+            binding,
+            width: definition.width,
+            height: definition.base.size,
+        })
+    }
+
+    pub fn render(
+        &self,
+        queue: &wgpu::Queue,
+        render_pass: &mut wgpu::RenderPass<'_>,
+        resolution: (u32, u32),
+        card: &Card,
+    ) {
+        queue.write_buffer(
+            &self.uniforms,
+            0,
+            bytemuck::cast_slice(&[Uniforms {
+                resolution: [resolution.0, resolution.1],
+            }]),
+        );
+
+        render_pass.set_pipeline(&self.raw);
+        render_pass.set_bind_group(0, &self.uniforms_binding, &[]);
+        render_pass.set_bind_group(1, &card.binding, &[]);
+        render_pass.set_vertex_buffer(0, card.instance.slice(..));
+        render_pass.draw(0..6, 0..1);
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Card {
+    width: u32,
+    height: u32,
+    instance: wgpu::Buffer,
+    base: wgpu::Texture,
+    foil: Option<wgpu::Texture>,
+    etching: Option<wgpu::Texture>,
+    binding: wgpu::BindGroup,
+}
+
+impl Card {
+    pub fn prepare(&mut self, queue: &wgpu::Queue, x: f32, y: f32, rotation: f32) {
+        queue.write_buffer(
+            &self.instance,
+            0,
+            bytemuck::cast_slice(&[Instance {
+                position: [x, y],
+                size: [self.width, self.height],
+                rotation,
+            }]),
+        );
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Structure {
+    pub base: Layer,
+    pub foil: Option<Layer>,
+    pub etching: Option<Layer>,
+    pub width: u32,
+}
+
+#[derive(Debug, Clone)]
+pub struct Layer {
+    pub rgba: Bytes,
+    pub size: u32,
+}
+
+impl Layer {
+    fn upload(&self, device: &wgpu::Device, queue: &wgpu::Queue) -> wgpu::Texture {
+        use wgpu::util::DeviceExt;
+
+        device.create_texture_with_data(
+            queue,
+            &wgpu::TextureDescriptor {
+                label: None,
+                size: wgpu::Extent3d {
+                    width: self.size,
+                    height: self.size,
+                    depth_or_array_layers: 1,
+                },
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsages::TEXTURE_BINDING,
+                view_formats: &[],
+            },
+            wgpu::util::TextureDataOrder::LayerMajor,
+            &self.rgba,
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy, bytemuck::Zeroable, bytemuck::Pod)]
+#[repr(C)]
+struct Instance {
+    position: [f32; 2],
+    size: [u32; 2],
+    rotation: f32,
+}
+
+#[derive(Debug, Clone, Copy, bytemuck::Zeroable, bytemuck::Pod)]
+#[repr(C)]
+struct Uniforms {
+    resolution: [u32; 2],
+}
---
+++ src/shader.wgsl
@@ -0,0 +1,155 @@
+struct Uniforms {
+    resolution: vec2<u32>,
+}
+
+@group(0) @binding(0) var<uniform> uniforms: Uniforms;
+@group(0) @binding(1) var u_sampler: sampler;
+@group(0) @binding(2) var u_back: texture_2d<f32>;
+
+@group(1) @binding(0) var u_base: texture_2d<f32>;
+@group(1) @binding(1) var u_foil: texture_2d<f32>;
+@group(1) @binding(2) var u_etch: texture_2d<f32>;
+
+struct VertexInput {
+    @location(0) position: vec2<f32>,
+    @location(1) size: vec2<u32>,
+    @location(2) rotation: f32,
+    @builtin(vertex_index) index: u32,
+}
+
+struct VertexOutput {
+    @builtin(position) position: vec4<f32>,
+    @location(0) center: vec2<f32>,
+    @location(1) size: vec2<f32>,
+    @location(2) rotation: f32,
+}
+
+@vertex
+fn vs_main(input: VertexInput) -> VertexOutput {
+    var out: VertexOutput;
+
+    let corner = corner_position(input.index);
+    let position = vec2<f32>(input.size * corner) + (input.position + vec2<f32>(uniforms.resolution) / 2.0) - vec2<f32>(input.size) / 2.0;
+
+    out.center = vec2<f32>(input.position);
+    out.size = vec2<f32>(input.size);
+    out.rotation = input.rotation;
+    out.position = vec4<f32>(2.0 * position / vec2<f32>(uniforms.resolution) - 1.0, 0.0, 1.0);
+
+    return out;
+}
+
+@fragment
+fn fs_main(input: VertexOutput) -> @location(0) vec4<f32> {
+    const n_samples: i32 = 4;
+    const max_distance: f32 = 1.0;
+
+    let camera = vec3(0.0, 0.0, -max_distance * 2.0);
+    let fov = 45.0; // degrees
+    let aspect = f32(uniforms.resolution.x) / f32(uniforms.resolution.y);
+    let ndc = vec2(
+        (input.position.x / f32(uniforms.resolution.x)) * 2.0 - 1.0,
+        1.0 - (input.position.y / f32(uniforms.resolution.y)) * 2.0
+    );
+
+    let screen_pos = vec3(ndc.x * aspect * tan(radians(fov)/2.0),
+                          ndc.y * tan(radians(fov)/2.0),
+                          1.0);
+
+    let position = input.position.xy - vec2<f32>(uniforms.resolution) / 2.0 + input.center;
+    let max_dimension = f32(max(input.size.x, input.size.y));
+    let card_size = input.size / (2.0 * max_dimension);
+
+    let cos_rot = cos(input.rotation);
+    let sin_rot = sin(input.rotation);
+
+    let rotation = mat3x3<f32>(
+        vec3<f32>(cos_rot, 0.0, sin_rot),
+        vec3<f32>(0.0, 1.0, 0.0),
+        vec3<f32>(-sin_rot, 0.0, cos_rot),
+    );
+
+    var color: vec4<f32>;
+
+    for (var m = 0; m < n_samples; m++) {
+    for (var n = 0; n < n_samples; n++) {
+        let o = vec2(f32(m), f32(n)) / f32(n_samples) - 0.5;
+        let ray_origin = vec3((position + o) / max_dimension, -max_distance);
+        let ray_target = vec3(position / max_dimension, max_distance);
+        let ray_direction = normalize(ray_target - ray_origin);
+
+        var t = -max_distance;
+
+        for (var i = 0; i < 64; i++) {
+            let p = transpose(rotation) * (ray_origin + ray_direction * t);
+            let d = sd_card(p, card_size);
+
+            if d < 0.001 || t > 2.0 * max_distance {
+                break;
+            }
+
+            t += d;
+        }
+
+
+        if t <= 2.0 * max_distance {
+            let hit = transpose(rotation) * (ray_origin + ray_direction * t);
+            let normal = estimate_normal(hit, card_size);
+            let normal_abs = abs(normal);
+
+            if (normal_abs.z > normal_abs.x && normal_abs.z > normal_abs.y) {
+                let local_uv = hit.xy / (2.0 * card_size) + vec2(0.5, 0.5);
+                let uv_offset = vec2(0.5, 0.5) - card_size;
+                let final_uv = uv_offset + local_uv * card_size * 2.0;
+
+                if (normal.z < 0.0) {
+                    // Front
+                    color += textureSampleLevel(u_base, u_sampler, final_uv, 0.0);
+                } else {
+                    // Back
+                    color += textureSampleLevel(u_back, u_sampler, vec2(1.0 - final_uv.x, final_uv.y), 0.0);
+                }
+            } else if (normal * rotation).z > 0.0 {
+                // Side edge
+                color += vec4(0.5, 0.5, 0.5, 1.0);
+            }
+        }
+    }
+    }
+
+    color /= f32(n_samples * n_samples);
+
+    return color;
+}
+
+// Compute the normalized quad coordinates based on the vertex index.
+fn corner_position(vertex_index: u32) -> vec2<u32> {
+    // #: 0 1 2 3 4 5
+    // x: 1 1 0 0 0 1
+    // y: 1 0 0 0 1 1
+    return vec2<u32>((vec2(1u, 2u) + vertex_index) % vec2(6u) < vec2(3u));
+}
+
+fn sd_card(p: vec3<f32>, size: vec2<f32>) -> f32 {
+    return extrude(p, sd_rounded_box(p.xy, size, size.x / 20.0), size.x / 220.0);
+}
+
+fn sd_rounded_box(p: vec2<f32>, b: vec2<f32>, r: f32) -> f32 {
+    let q = abs(p) - b + vec2(r);
+    return min(max(q.x, q.y), 0.0) + length(max(q, vec2(0.0))) - r;
+}
+
+fn extrude(p: vec3<f32>, sdf: f32, h: f32) -> f32 {
+    let w = vec2(sdf, abs(p.z) - h);
+  	return min(max(w.x, w.y), 0.0) + length(max(w, vec2(0.0)));
+}
+
+fn estimate_normal(p: vec3<f32>, size: vec2<f32>) -> vec3<f32> {
+    let eps = 0.0001;
+
+    return normalize(vec3(
+        sd_card(p + vec3(eps, 0, 0), size) - sd_card(p - vec3(eps, 0, 0), size),
+        sd_card(p + vec3(0, eps, 0), size) - sd_card(p - vec3(0, eps, 0), size),
+        sd_card(p + vec3(0, 0, eps), size) - sd_card(p - vec3(0, 0, eps), size)
+    ));
+}
